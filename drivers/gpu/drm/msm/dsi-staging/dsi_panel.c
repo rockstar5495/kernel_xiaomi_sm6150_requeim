8c15d227aff700736bae1c191bc648b1324a24bd
Rasenkai <rasenkai99@gmail.com>
2021-08-25 20:15:10 +0530

    drm: msm: Add high brightness mode (HBM) support for LCD devices
    * well, phoenix also has HBM...
    
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>
    Change-Id: Ieed3dcab8925a52db1013391b57be79bb9103c3e

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1868,6 +1868,9 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-doze-lbm-command",
 	"qcom,mdss-dsi-dispparam-hbm-fod-on-command",
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-command",
+	"qcom,mdss-dsi-dispparam-lcd-hbm-l1-on-command",
+	"qcom,mdss-dsi-dispparam-lcd-hbm-l2-on-command",
+	"qcom,mdss-dsi-dispparam-lcd-hbm-off-command",
 };
 
 const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
@@ -1898,6 +1901,9 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-doze-lbm-command-state",
 	"qcom,mdss-dsi-dispparam-hbm-fod-on-command-state",
 	"qcom,mdss-dsi-dispparam-hbm-fod-off-command-state",
+	"qcom,mdss-dsi-dispparam-lcd-hbm-l1-on-command-state",
+	"qcom,mdss-dsi-dispparam-lcd-hbm-l2-on-command-state",
+	"qcom,mdss-dsi-dispparam-lcd-hbm-off-command-state",
 };
 
 static int dsi_panel_get_cmd_pkt_count(const char *data, u32 length, u32 *cnt)
@@ -4661,8 +4667,14 @@ error:
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel)
 {
 	static const enum dsi_cmd_set_type type_map[] = {
+#if (defined CONFIG_MACH_XIAOMI_DAVINCI) || (defined CONFIG_MACH_XIAOMI_TOCO)
 		DSI_CMD_SET_DISP_HBM_FOD_OFF,
 		DSI_CMD_SET_DISP_HBM_FOD_ON
+#else
+		DSI_CMD_SET_DISP_LCD_HBM_OFF,
+		DSI_CMD_SET_DISP_LCD_HBM_L1_ON,
+		DSI_CMD_SET_DISP_LCD_HBM_L2_ON,
+#endif
 	};
 
 	enum dsi_cmd_set_type type;
